930444929bea971bb5a4e6f8a48b4168f981f1d3
cagriturkmen <c.turkmen2@hotmail.com>
2021-12-23 10:22:50 +0300

    some commits

---
+++ js/tutorials_javascript.js
@@ -0,0 +1,131 @@
+//js comment
+/*multiple comment*/
+// window.alert("Ekranda göster");
+// alert("kısaltılmış hali");
+// console.log("console log");
+// console.info("console info");
+// console.error("console error");
+
+// document.write("document write yazdım")
+//******************************************************* */
+
+//type
+//var sayi="kelime";
+//sayi=true;
+//sayi=34.56;
+//sayi=44;
+
+//alert(typeof sayi);
+/************************************************************* */
+
+// var sayi2="44",result;
+// var sayi1="22"
+// //result= sayi2/22;
+// alert(sayi2+ "/" +sayi1);
+
+/*********************************************************** */
+
+//global değişkenler için kullanalım
+// var deneme1 = "485";
+// deneme1 = true;
+
+// //local değişkenler
+// let deneme2 = "5555";
+// deneme2 = true;
+
+//değişken değerini değiştiremezsiniz
+//const deneme3 = "5555";
+
+/************************************************************** */
+
+// var object={
+//     adi:"Hamit",
+//     soyadi:"Mızrak",
+//     teknoloji:["Java","JSP"],
+// };
+// console.log(object);
+// console.log(object.adi);
+/************************************************************* */
+// var sayi=44/"asd";
+// console.log(sayi);
+// NaN
+// var sayi2;
+// console.log(sayi2);
+// undefined
+
+/****************************************************** */
+//JS:
+var sayi1="12";
+var sayi2=12;
+// console.log(sayi1>=sayi2);
+
+// sayi1++;
+// sayi2--;
+// if(sayi1==sayi2){
+//     console.log("eşit")
+// }else{
+//     console.log("eşit değil")
+// }
+
+// if(sayi1===sayi2){
+//     console.log("eşit")
+// }else{
+//     console.log("eşit değil")
+// }
+
+/**********************************************////////////// */
+
+//scanner
+//JOptionalPane.show...
+//dikkaaatt promptan gelen veri her zaman String'tir.
+// var sayi=prompt("Lütfen sayi giriniz");
+// console.log(Math.pow(sayi,2));
+// console.log(sayi**=2);
+
+//Math
+// var dizi= [1,2,3,4,5,6]
+// console.log(Math.abs(-4))
+// console.log(Math.sqrt(25))
+// console.log(Math.pow(2,5))
+// console.log(Math.min(dizi))
+// console.log(Math.max(-4,75,5,45,12,123))
+// console.log(Math.random()*4+1)
+
+//escape character
+// alert("\"")
+// console.log(4/"sa")   -->NaN     
+// var sayi2;
+// console.log(sayi2)           --->undefined
+// console.log(4/0)         -->infinity
+
+// try {
+//     let sayi=5/3;
+// } catch (e) {
+//     alert(e.message());
+// }finally{
+//     alert("Burası kesin kapatılacak");
+// }
+
+// //sayi
+// var sayi=44e-5;
+// sayi=44e6;
+// console.log(sayi)
+
+// //sayı sistemleri
+// var sayi2=0xFF;
+// //sayi2=0b11;
+// console.log(sayi2)
+
+//metinsel
+// // var kelime = " javascript";
+// // console.log("length:" + kelime.length);
+// // console.log("küçük harf: " + kelime.tolowercase());
+// // console.log("küçük harf: " + kelime.touppercase());
+// // console.log("indexof:" + kelime.indexof("j"));
+// // console.log("lastindexof:" + kelime.lastindexof("j"));
+// // console.log(kelime.trim().length);
+// // console.log(kelime.substring(4));
+// // console.log(kelime.substring(0,4));
+// // console.log(kelime.charat(1));
+// // console.log(kelime.replace(kelime,"yenisi"));
+// // console.log(kelime.search("j"));
